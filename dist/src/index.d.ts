254e75a5337f7aeef25b846f044108400d791ec5
ssribeiro <luan@ssribeiro.com>
2018-09-26 11:03:11 -0300

    added types

---
+++ dist/src/index.d.ts
@@ -0,0 +1,7 @@
+export declare const log: (t: any) => void;
+export declare const info: (t: any) => void;
+export declare const warn: (t: any) => void;
+export declare const error: (t: any) => void;
+export declare const success: (t: any) => void;
+export declare const dev: (t: any) => void;
+export declare const bp: (t: any) => void;
