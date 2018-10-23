c77efdc053c0be18e207856cf65a9b0911aa16aa
ssribeiro <luan@ssribeiro.com>
2018-10-23 18:29:35 -0300

    added type for delay

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -5,3 +5,4 @@ export declare const error: (t: any) => void;
 export declare const success: (t: any) => void;
 export declare const dev: (t: any) => void;
 export declare const bp: (t: any) => void;
+export declare const delay: (time: number) => Promise<void>;
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const chalk_1 = require("chalk");
 /* color dev console tools */
@@ -12,4 +20,7 @@ exports.dev = (t) => {
         console.log(chalk_1.default.bgCyan(t));
 };
 exports.bp = (t) => { console.warn(chalk_1.default.bgMagenta(t)); };
+exports.delay = (time) => __awaiter(this, void 0, void 0, function* () {
+    yield new Promise((resolve) => setTimeout(resolve, time));
+});
 //# sourceMappingURL=index.js.map
